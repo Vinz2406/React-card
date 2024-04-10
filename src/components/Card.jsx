bc778d1c780e122bcf0529c7e0118e31d48d485f
Vinz2406 <161700655+Vinz2406@users.noreply.github.com>
2024-04-10 16:12:49 +0200

    Espace retiré Sangoku

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ function Card() {
         <img src="https://i.pinimg.com/736x/a7/37/c7/a737c7f44673f99bfa37fd9e92eedc6e.jpg" alt="" />
         <div className="card-content">
           <h2>
-            SAN GOKU
+            SANGOKU
           </h2>
           <p>
           Son Goku est un Saiyan puissant connu pour sa personnalité joyeuse et son amour du combat. Il cherche constamment à devenir plus fort et à protéger ses proches. Il développe des techniques de combat redoutables comme le Kamehameha.
